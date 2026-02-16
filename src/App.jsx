386d371623c1438df5e4788c05d9e682456f31fb
rajputayus7 <ayush.rajput@applore.in>
2026-02-16 22:51:10 +0530

    homepage route added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/reset-password" element={<PasswordReset />} />
+        <Route path="/" element={<div> HomePage </div>} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
